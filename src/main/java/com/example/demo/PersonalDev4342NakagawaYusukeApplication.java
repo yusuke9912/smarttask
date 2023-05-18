6dbe28fd62f2e92db29970d7a5151ea57ba67692
Yusuke Nakagawa <nakagawa.yusuke.4342@gmail.com>
2023-05-18 09:39:19 +0900

    First Commit

---
+++ src/main/java/com/example/demo/PersonalDev4342NakagawaYusukeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev4342NakagawaYusukeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev4342NakagawaYusukeApplication.class, args);
+	}
+
+}
