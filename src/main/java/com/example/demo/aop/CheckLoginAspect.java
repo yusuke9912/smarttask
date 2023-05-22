6516355ca1bc73c0688cf3634bdc5a3ea798378f
Yusuke Nakagawa <nakagawa.yusuke.4342@gmail.com>
2023-05-22 09:30:49 +0900

    update : ログインしていない場合は、タスク画面はアクセスできないように変更

---
+++ src/main/java/com/example/demo/aop/CheckLoginAspect.java
@@ -0,0 +1,25 @@
+package com.example.demo.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.Account;
+
+@Aspect
+@Component
+public class CheckLoginAspect {
+	@Autowired
+	Account account;
+
+	@Around("execution(* com.example.demo.controller.TaskController.*(..))")
+	public Object checkLogin(ProceedingJoinPoint jp) throws Throwable {
+		if (account == null || account.getName() == null || account.getName().length() == 0) {
+			return "redirect:/";
+		}
+		return jp.proceed();
+
+	}
+}
